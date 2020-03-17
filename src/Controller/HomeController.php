975340159cd79e495979c957a91b36ef836573fd
alina <alina-giggles@yahoo.com>
2020-03-17 09:00:47 +0100

    menu client

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -49,4 +49,43 @@ class HomeController extends AbstractController
     //     return $this->render('home/home.html.twig', compact("recettes"));
     // }
 
+
+ //------------------------------MENU---------------------------------------------------------
+    /**
+     * @Route("/visage", name="visage")
+     */
+    public function categVisage(RecetteRepository $recetteRepo, CategorieRepository $catRepo)
+    {
+        $categorie = 3;
+        $visage = $catRepo->find($categorie);
+        $categVisage= $visage->getRecettes(); 
+
+        return $this->render('categories/visage.html.twig', compact("categVisage") );
+    }
+
+
+    /**
+     * @Route("/cheveux", name="cheveux")
+     */
+    public function categCheveux(RecetteRepository $recetteRepo, CategorieRepository $catRepo)
+    {
+        $categorie = 2;
+        $cheveux = $catRepo->find($categorie);
+        $categCheveux= $cheveux->getRecettes(); 
+
+        return $this->render('categories/cheveux.html.twig', compact("categCheveux") );
+    }
+
+    /**
+     * @Route("/corps", name="corps")
+     */
+    public function categCorps(RecetteRepository $recetteRepo, CategorieRepository $catRepo)
+    {
+        $categorie = 1;
+        $corps = $catRepo->find($categorie);
+        $categCorps= $corps->getRecettes(); 
+
+        return $this->render('categories/corps.html.twig', compact("categCorps") );
+    }
+
 }

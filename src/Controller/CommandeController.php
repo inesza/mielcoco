71370b83dbf0a82ac131164f2aadc4c1dd499d16
alina <alina-giggles@yahoo.com>
2020-03-19 09:52:50 +0100

    lien commande

---
+++ src/Controller/CommandeController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface as EMI;
+use Symfony\Component\HttpFoundation\Request;
+use App\Repository\CommandeRepository;
+use App\Repository\ClientRepository;
+use App\Entity\Commande;
+use App\Entity\Client;
+use App\Form\CommandeType;
+
+class CommandeController extends AbstractController
+{
+    /**
+     * @Route("/admin/commande", name="liste_commande")
+     */
+    public function liste(CommandeRepository $cr)
+    {
+    
+        $commandes = $cr->findAll();
+    
+        $taille = count($commandes);
+        return $this->render('commande/index.html.twig', [
+            "commandes" => $commandes, "taille" => $taille
+        ]);
+
+    }
+
+    /**
+     * @Route("/admin/commande/modifier/{id}", name="commande_modifier", requirements={"id" = "\d+"})
+     */
+
+    public function modifier(CommandeRepository $cr, EMI $em, Request $rq, int $id)
+    {
+        $commandeModif = $cr->find($id);
+        $dateCommande = $commandeModif->getDate();
+        $formCommande = $this->createForm(CommandeType::class, $commandeModif);
+        $commandeModif->setDate($dateCommande);
+
+        $formCommande->handleRequest($rq);
+        if($formCommande->isSubmitted()) {
+            if($formCommande->isValid()) {
+                $em->persist($commandeModif); 
+                $em->flush();   
+                $this->addFlash("success", "Modification bien enregistrée"); 
+                return $this->redirectToRoute("liste_commande");
+            } else {
+                $this->addFlash("danger", "Le formulaire n'est pas valide");
+            }
+        }
+        $formCommande = $formCommande->createView();  
+        return $this->render('commande/formCommande.html.twig', ["formCommande" => $formCommande, "commande" => $commandeModif] );
+    }
+}
---
+++ src/Form/CommandeType.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type;
+
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+
+class CommandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            // ->add('client', IntegerType::class )
+            // ->add('montant', TextType::class, [ "attr" => [ "readonly" => true, "class" => "form-control-plaintext" ] ])
+            //->add('date', DateType::class, [ "widget" => "single_text", "attr" => [ "disabled" => true ], "input" => "datetime", "required" => false ])
+            ->add('etat', ChoiceType::class, [
+                "attr" => ["class" => "form-check-inline"],
+                'choices'  => [
+                    'En cours de traitement' => "traitement",
+                    'En cours de livraison' => "livraison",
+                    'Expédié' => "expedie"
+                    ]
+            ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
